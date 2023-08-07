dea67dedae817c713f5edb5b1372955b2ece9d9e
Dilip Kumar S <106368038+Dilipkumar1401@users.noreply.github.com>
2023-08-07 15:46:25 +0530

    SIMPLE GAME USING OOPS

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        player1 player=new player1("dilip","sword",100);
+//        System.out.println(player.getName());
+//        System.out.println(player.getHealth());
+//        System.out.println(player.getWeapon());
+       player.damageByGun1();
+       player.damageByGun1();
+       player.damageByGun2();
+        player.heal();
+
+//        player2 betterplayer=new player2("rak","machine gun",100,false);
+//        betterplayer.damageByGun1();
+    }
+}
